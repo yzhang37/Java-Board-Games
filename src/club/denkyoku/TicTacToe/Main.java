56c133e11ecd790311eda89373276446fc8adcc4
青椒 <10142130151_ecnu@outlook.com>
2022-02-10 10:58:29 -0500

    完成对话框的代码

--- src/club/denkyoku/TicTacToe/Main.java
+++ src/club/denkyoku/TicTacToe/Main.java
@@ -6,8 +6,11 @@ public class Main {
         SttyHelper.disableEcho();
         SttyHelper.bufferByCharacter();
 
-        TicTacToe game = new TicTacToe();
-        game.start();
+        MessageDialog.show(new String[]{"Do you want to play TicTacToe?"},
+                MessageDialog.getYesNo(), 0, -1);
+
+//        TicTacToe game = new TicTacToe();
+//        game.start();
 
         // enable echo before exit
         SttyHelper.enableEcho();
--- src/club/denkyoku/TicTacToe/MessageDialog.java
+++ src/club/denkyoku/TicTacToe/MessageDialog.java
@@ -1,11 +1,244 @@
 package club.denkyoku.TicTacToe;
 
+import java.util.ArrayList;
+
 public class MessageDialog {
-    public static int show(String message) {
-        return show(message, new String[]{"OK"}, 0);
+    public static class Button {
+        private final String title;
+        private final char accessKey;
+
+        public Button(String title, char accessKey) {
+            this.title = title;
+            if ('0' <= accessKey && accessKey <= '9' ||
+                    'A' <= accessKey && accessKey <= 'Z') {
+                this.accessKey = accessKey;
+            } else if ('a' <= accessKey && accessKey <= 'z') {
+                this.accessKey = (char) (accessKey - ('a' - 'A'));
+            } else {
+                this.accessKey = '\0';
+            }
+        }
+
+        public String getTitle() {
+            return this.title;
+        }
+
+        public char getAccessKey() {
+            return this.accessKey;
+        }
     }
 
-    public static int show(String message, String[] buttons, int defaultButton) {
+    public static Button[] getOKCancel() {
+        return new Button[]{
+                new Button("OK", '\0'),
+                new Button("Cancel", '\0')
+        };
+    }
+
+    public static Button[] getYesNo() {
+        return new Button[]{
+                new Button("Yes", 'Y'),
+                new Button("No", 'N')
+        };
+    }
+
+    public static Button[] getYesNoCancel() {
+        return new Button[]{
+                new Button("Yes", 'Y'),
+                new Button("No", 'N'),
+                new Button("Cancel", '\0')
+        };
+    }
+
+    public static Button[] getOK() {
+        return new Button[]{
+                new Button("OK", '\0')
+        };
+    }
+
+    public static int show(String[] message) {
+        return show(message, getOK(), 0, -1);
+    }
+
+    public static int show(String[] message, Button[] buttons, int defaultButton, int cancelButton) {
+        String[] lastScreen = ConsoleHelper.GetLastScreen();
+
+        int currentButton = defaultButton;
+        printDialog(message, buttons, currentButton, lastScreen);
+
         return -1;
     }
+
+    protected static void printDialog(String[] message, Button[] buttons, int currentButton, String[] lastScreen) {
+        // 首先获取当前窗口的宽度和高度
+        int height = ConsoleHelper.GetConsoleHeight(),
+                width = ConsoleHelper.GetConsoleWidth();
+
+        // 先计算所需要的高度
+        int neededHeight = buttons.length + 1 + message.length;
+        boolean showTopBottomBorder = false, showTopBottomMargin = false;
+
+        // 如果增加边框可以显示，上下各增加边框高度
+        if (neededHeight + 2 < height) {
+            neededHeight += 2;
+            showTopBottomBorder = true;
+        }
+        if (neededHeight + 2 < height) {
+            neededHeight += 2;
+            showTopBottomMargin = true;
+        }
+
+        // 获取所有文本中最宽的
+        int neededWidth = 0;
+        boolean showLeftRightBorder = false, showLeftRightMargin = false;
+        for (String s : message) {
+            if (s.length() > neededWidth) {
+                neededWidth = s.length();
+            }
+        }
+        for (Button b : buttons) {
+            int length = b.getTitle().length() + 2;
+            if (length > neededWidth) {
+                neededWidth = length;
+            }
+        }
+        // 如果增加边框可以显示，左右各增加边框宽度
+        if (neededWidth + 2 < width) {
+            neededWidth += 2;
+            showLeftRightBorder = true;
+        }
+        if (neededWidth + 2 < width) {
+            neededWidth += 2;
+            showLeftRightMargin = true;
+        }
+
+        // 获取 Dialog 实际的左上角坐标
+        int dialog_Y = Math.max(0, (width - neededWidth) / 2),
+                dialog_X = Math.max(0, (height - neededHeight) / 2);
+
+        // 先获取之前的 screen，然后修改它。
+        ArrayList<String> newScreen = new ArrayList<String>();
+        int lines = 0;
+        while (lines < dialog_X) {
+            if (lastScreen.length <= lines)
+                newScreen.add("");
+            else
+                newScreen.add(lastScreen[lines]);
+            lines++;
+        }
+
+        // 替换文本为窗口
+        int dialogLines = 0;
+        String leftTopCorner = "", rightTopCorner = "",
+                leftBottomCorner = "", rightBottomCorner = "",
+                leftMargin = "", rightMargin = "";
+
+        if (showLeftRightBorder) {
+            leftTopCorner = "┌";
+            rightTopCorner = "┒";
+            leftMargin = "│";
+            rightMargin = "┃";
+            leftBottomCorner = "┕";
+            rightBottomCorner = "┛";
+        }
+        if (showLeftRightMargin) {
+            leftTopCorner += "─";
+            rightTopCorner = "─" + rightTopCorner;
+            leftMargin += " ";
+            rightMargin = " " + rightMargin;
+            leftBottomCorner += "━";
+            rightBottomCorner = "━" + rightBottomCorner;
+        }
+        if (dialog_Y > 0) {
+            leftTopCorner = " ".repeat(dialog_Y) + leftTopCorner;
+            leftMargin = " ".repeat(dialog_Y) + leftMargin;
+            leftBottomCorner = " ".repeat(dialog_Y) + leftBottomCorner;
+        }
+
+        int curMessage = 0;
+        int curButton = 0;
+        while (dialogLines < neededHeight) {
+            if (showTopBottomBorder && dialogLines == 0) {
+                if (showLeftRightBorder && showLeftRightMargin) {
+                    newScreen.add(leftTopCorner + "─".repeat(neededWidth - 4) + rightTopCorner);
+                } else if (showLeftRightBorder) {
+                    newScreen.add(leftTopCorner + "─".repeat(neededWidth - 2) + rightTopCorner);
+                } else {
+                    newScreen.add("─".repeat(neededWidth));
+                }
+            } else if (showTopBottomMargin && dialogLines == 1) {
+                if (showLeftRightBorder && showLeftRightMargin) {
+                    newScreen.add(leftMargin + " ".repeat(neededWidth - 4) + rightMargin);
+                } else if (showLeftRightBorder) {
+                    newScreen.add(leftMargin + " ".repeat(neededWidth - 2) + rightMargin);
+                } else {
+                    newScreen.add(" ".repeat(neededWidth));
+                }
+            } else if (showTopBottomMargin && dialogLines == neededHeight - 2) {
+                if (showLeftRightBorder && showLeftRightMargin) {
+                    newScreen.add(leftMargin + " ".repeat(neededWidth - 4) + rightMargin);
+                } else if (showLeftRightBorder) {
+                    newScreen.add(leftMargin + " ".repeat(neededWidth - 2) + rightMargin);
+                } else {
+                    newScreen.add(" ".repeat(neededWidth));
+                }
+            } else if (showTopBottomBorder && dialogLines == neededHeight - 1) {
+                if (showLeftRightBorder && showLeftRightMargin) {
+                    newScreen.add(leftBottomCorner + "━".repeat(neededWidth - 4) + rightBottomCorner);
+                } else if (showLeftRightBorder) {
+                    newScreen.add(leftBottomCorner + "━".repeat(neededWidth - 2) + rightBottomCorner);
+                } else {
+                    newScreen.add("━".repeat(neededWidth));
+                }
+            } else {
+                if (curMessage < message.length) {
+                    String s = message[curMessage];
+                    if (showLeftRightBorder && showLeftRightMargin) {
+                        newScreen.add(leftMargin + s + " ".repeat(Math.max(0, neededWidth - 4 - s.length())) + rightMargin);
+                    } else if (showLeftRightBorder) {
+                        newScreen.add(leftMargin + s + " ".repeat(Math.max(0, neededWidth - 2 - s.length())) + rightMargin);
+                    } else {
+                        newScreen.add(s);
+                    }
+                    curMessage++;
+                } else if (curMessage == message.length) {
+                    if (showLeftRightBorder && showLeftRightMargin) {
+                        newScreen.add(leftMargin + "─".repeat(neededWidth - 4) + rightMargin);
+                    } else if (showLeftRightBorder) {
+                        newScreen.add(leftMargin + "─".repeat(neededWidth - 2) + rightMargin);
+                    } else {
+                        newScreen.add("─".repeat(neededWidth));
+                    }
+                    curMessage++;
+                } else if (curButton < buttons.length) {
+                    String prefix = "  ";
+                    if (currentButton == curButton) {
+                        prefix = "▶ ";
+                    }
+                    String s = buttons[curButton].getTitle();
+                    if (showLeftRightBorder && showLeftRightMargin) {
+                        newScreen.add(leftMargin + prefix + s + " ".repeat(Math.max(0, neededWidth - 2 - 4 - s.length())) + rightMargin);
+                    } else if (showLeftRightBorder) {
+                        newScreen.add(leftMargin + prefix + s + " ".repeat(Math.max(0, neededWidth - 2 - 2 - s.length())) + rightMargin);
+                    } else {
+                        newScreen.add(prefix + s);
+                    }
+                    curButton++;
+                }
+            }
+
+            dialogLines++;
+            lines++;
+        }
+
+        while (lines < lastScreen.length) {
+            newScreen.add(lastScreen[lines]);
+            lines++;
+        }
+
+        String[] newScreenArray = new String[newScreen.size()];
+        newScreen.toArray(newScreenArray);
+        ConsoleHelper.printScreen(newScreenArray);
+    }
+
 }
