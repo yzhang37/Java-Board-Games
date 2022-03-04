2ca7f2d09ae5c9a259cdcfd69d851a641fe81c89
青椒 <johnyin@bu.edu>
2022-03-04 05:04:26 -0500

    fix character problems under windows

--- src/club/denkyoku/tictactoe/services/output/terminal/ConsoleHelper.java
+++ src/club/denkyoku/tictactoe/services/output/terminal/ConsoleHelper.java
@@ -4,38 +4,83 @@ import club.denkyoku.tictactoe.libraries.os.RawConsoleInput;
 import club.denkyoku.tictactoe.libraries.os.ShellHelper;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 
 public class ConsoleHelper {
-    // TODO: add support for Windows, to display Unicodes.
+    // Used for Linux to output UTF-8
+    protected static final PrintWriter printWriterUnix = new PrintWriter(System.out, true);
 
-    protected static final PrintWriter printWriter = new PrintWriter(System.out, true);
+    // Used for Windows to output UTF-8
+    protected static PrintStream outStreamWin = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
     protected static String[] lastScreen = new String[]{""};
+
+    private static boolean windowsRunOnce = true;
+
+    private static void winRunOnce() {
+        if (windowsRunOnce) {
+            try {
+                new ProcessBuilder("cmd", "/c", "chcp 65001").inheritIO().start().waitFor();
+            } catch (IOException | InterruptedException e) {
+            }
+            windowsRunOnce = false;
+        }
+    }
 
     /**
      * Helper method to clear the console.
      */
     public static void CleanConsole() {
-        if (RawConsoleInput.getIsWindows()){
+        if (RawConsoleInput.getIsWindows()) {
             try {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } catch (IOException | InterruptedException e) {}
+            } catch (IOException | InterruptedException e) {
+            }
         } else {
-            printWriter.print("\033[H\033[2J");
+            printWriterUnix.print("\033[H\033[2J");
         }
     }
 
+    /**
+     * Helper method to print UTF-8 into the console.
+     * @param str The string to print.
+     */
     public static void println(String str) {
-        printWriter.println(str);
+        if (RawConsoleInput.getIsWindows()) {
+            winRunOnce();
+            outStreamWin.println(str);
+        } else {
+            printWriterUnix.println(str);
+        }
     }
 
+    /**
+     * Helper method to print UTF-8 into the console.
+     * @param str The string to print.
+     */
     public static void print(String str) {
-        printWriter.print(str);
+        if (RawConsoleInput.getIsWindows()) {
+            winRunOnce();
+            outStreamWin.print(str);
+        } else {
+            printWriterUnix.print(str);
+        }
     }
 
+    /**
+     * Helper method to print UTF-8 into the console.
+     * @param c The character to print.
+     */
     public static void print(char c) {
-        printWriter.print(c);
+        if (RawConsoleInput.getIsWindows()) {
+            winRunOnce();
+            outStreamWin.print(c);
+        } else {
+            printWriterUnix.print(c);
+        }
     }
 
     /**
@@ -51,6 +96,7 @@ public class ConsoleHelper {
      * <br/>
      * If you want to do screen restore, please use <code>GetLastScreen()</code> method
      * before this call.
+     *
      * @param screen The new screen strings.
      */
     public static void printScreen(String[] screen) {
@@ -63,8 +109,9 @@ public class ConsoleHelper {
 
     /**
      * Get the previous screen strings.
-     *
+     * <p>
      * Useful when you want to restore the screen after a screen change.
+     *
      * @return The previous screen strings.
      */
     public static String[] GetLastScreen() {
@@ -73,6 +120,7 @@ public class ConsoleHelper {
 
     /**
      * Helper method to get the console width.
+     *
      * @return The width of console window.
      */
     public static int GetConsoleWidth() {
@@ -82,12 +130,14 @@ public class ConsoleHelper {
             if (result.ret == 0) {
                 return Integer.parseInt(result.output);
             }
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        }
         return 80; //default value
     }
 
     /**
      * Helper method to get the console height.
+     *
      * @return The height of console window.
      */
     public static int GetConsoleHeight() {
@@ -97,7 +147,8 @@ public class ConsoleHelper {
             if (result.ret == 0) {
                 return Integer.parseInt(result.output);
             }
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        }
         return 24; //default value
     }
 }
